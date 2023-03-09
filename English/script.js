ae6cf9b51745ed73c4146bc711d1bcd13779866f
JagaNatarajan <120781427+JagaNatarajan@users.noreply.github.com>
2023-03-09 13:50:07 +0530

    Add files via upload
    I have created a beginner friendly project in Vanilla JavaScript, HTML, CSS.
     A bookshop website is created, where it is preferred to buy books and a small divisions to practice English.

---
+++ English/script.js
@@ -0,0 +1,25 @@
+/* Quote generater */
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url ="https://api.quotable.io/random";
+
+let getquote = () => {
+    fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+        console.log(item);
+        console.log(item.author);
+        quote.innerText = item.content;
+       author.innerText = item.author;
+    });
+
+}
+window.addEventListener("Load", getquote);
+btn.addEventListener("click", getquote);
+
+
+
+
+
